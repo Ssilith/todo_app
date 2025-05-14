ca232353ac86ddc2432d4455e28a2f8c96f5baac
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2025-05-14 14:34:46 +0200

    fix

--- test/integration_test.dart
+++ test/integration_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'integration_tests/auth_flow_test.dart' as auth;
 import 'integration_tests/todo_flow_test.dart' as todo;
 
-import '../test/utils/benchmark_helper.dart';
+import 'utils/benchmark_helper.dart';
 import 'utils/supabase_test_setup.dart';
 
 void main() {
